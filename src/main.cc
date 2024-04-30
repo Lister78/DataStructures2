9d9bc903f1bf3906aa0597a221f31fecd75223fb
Lister78 <mateuszstochel4@gmail.com>
2024-04-30 18:46:20 +0200

    Tests of previous commit

--- src/main.cc
+++ src/main.cc
@@ -3,8 +3,15 @@
 int main()
 {
 	LinkedList lista;
-
-	std::cout<<"Initializatione went good";
+	Node* wezel;
+	lista.Insert(10,20);
+	lista.Insert(20,30);
+	lista.Insert(40,50);
+	lista.Insert(1,2);
+	std::cout<<"Wielkosc struktury: "<<lista.get_size()<<std::endl;
+	wezel=lista.peek();
+	std::cout<<"wartosc: "<<wezel->node_value<<" prio: "<<wezel->priority;
+	std::cout<<"\nInitializatione went good";
 
 	return 0;
 }
